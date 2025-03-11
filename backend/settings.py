b66186f1252496248e94c5aa417014019cf645e8
pinghow0220 <leepinghow0220@gmail.com>
2025-03-11 15:36:27 +0800

    Updated CORS settings

--- backend/settings.py
+++ backend/settings.py
@@ -53,7 +53,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://your-react-app.netlify.app",  # Change to your frontend URL
+]
+
 
 ROOT_URLCONF = 'backend.urls'
 
