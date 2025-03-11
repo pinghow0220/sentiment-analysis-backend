805eac2fe2b4c7502084413ff775a9f40a7e0f48
pinghow0220 <leepinghow0220@gmail.com>
2025-03-11 14:28:14 +0800

    Initial commit

---
+++ sentiment/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import sentiment_analysis
+
+urlpatterns = [
+    path('analyze/', sentiment_analysis, name='sentiment-analysis'),
+]
---
+++ sentiment/views.py
@@ -0,0 +1,27 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from textblob import TextBlob
+from nltk.sentiment import SentimentIntensityAnalyzer
+import nltk
+
+nltk.download('vader_lexicon')
+
+def analyze_sentiment(text):
+    sia = SentimentIntensityAnalyzer()
+    score = sia.polarity_scores(text)['compound']
+
+    if score > 0:
+        return "Positive 😊"
+    elif score == 0:
+        return "Neutral 😐"
+    else:
+        return "Negative 😢"
+    
+@api_view(['POST'])
+def sentiment_analysis(request):
+    text = request.data.get('text', '')
+    if not text:
+        return Response({'error': 'Please provide text to analyze'}, status=400)
+
+    sentiment = analyze_sentiment(text)
+    return Response({'sentiment': sentiment})
